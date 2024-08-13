e40b56cc11521fcf73e22cd337644d4a2e1d9888
alevivas <alejandro.vivas@dualbootpartners.com>
2024-08-13 08:50:01 -0300

    Update Chroma version

--- src/app.py
+++ src/app.py
@@ -1,16 +1,19 @@
 # pip install streamlit langchain lanchain-openai beautifulsoup4 python-dotenv chromadb
 
+import uuid
 import streamlit as st
 from langchain_core.messages import AIMessage, HumanMessage
 from langchain_community.document_loaders import WebBaseLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.vectorstores import Chroma
+from langchain.vectorstores import Chroma
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from dotenv import load_dotenv
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain.chains import create_history_aware_retriever, create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
-
+from langchain.prompts import PromptTemplate
+from langchain.chains import RetrievalQA
+from langchain.chains import VectorDBQA
 
 load_dotenv()
 
